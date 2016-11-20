1993b537d8c12df20cc7ca90fea81f85de74f730
Siddharth Sharma <sid.sharma0@gmail.com>
2016-11-20 16:45:01 -0500

    modified to include maxwalksat

--- code/6/optimize.py
+++ code/6/optimize.py
@@ -1,18 +1,17 @@
+from __future__ import print_function
 from O          import o 
 from model      import Model
 from sa         import optimize as sa_optimize 
+from maxwalksat	import optimize as mws_optimize
 
-
-def pretty_print( O ) :
-   for k,v in O : 
+def pretty_print(r) :
+   for k,v in r : 
       if( k.startswith("_") ) :
          continue
       print( " %25s : %s"%(k,v) ) 
     
 results = []
 for model in [ Model.schaffer(), Model.osyczka(), Model.kursawe() ]:
-    for optimizer in [sa_optimize]:
+    for optimizer in [sa_optimize, mws_optimize]:
         res = optimizer(model)
-        pretty_print( 
-
-
+        pretty_print(res) 
